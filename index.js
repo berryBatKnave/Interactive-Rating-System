c85dbbd7500d0a1ec930c27db6cbd93826c4a9d1
Yudhajit Chowdhury <110385709+berryBatKnave@users.noreply.github.com>
2023-07-31 19:52:41 +0530

    Interactive Rating System v1.0
    There was bug in the code for "tabIndex = 0".tabIndex only letting us to focus the "li" element but if we click out of the menu then the focus on the "li" is going away. Changed the functionality of the "li" with "tabIndex" to a "button" under the "li" tag.
    
    <li class="rating-1"><button>1</button></li>
    
    Basic functionality of the website is completed. Passed the value by editing the document and made the thank-you page under the same index.js with visiblity as "hidden".
    
    ********Release the Website as the basic functionality is covered.********

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var flag =false;
 var clickedP;
 
 $(".rating-elements li").click(function(){
-    console.log(this.innerHTML);
+    $(".rating-elements li button").css("background-color","hsl(218, 16%, 34%)").css("color","hsl(217, 12%, 63%)");
+    clicked = this.innerText;
+    $(".rating-"+clicked+" button").css("background-color","hsl(25, 97%, 53%)").css("color","white");
+    $(".submit-button").click(function(){
+        $("#rating-component").hide().css("visibility", "hidden");
+    })
+    $(".feedback").html(`Thank You For Rating Us ${clicked} out of 5`);
 });
 
-$(".button").click(function(){
-    console.log($("textarea#feedback").val());
-})
